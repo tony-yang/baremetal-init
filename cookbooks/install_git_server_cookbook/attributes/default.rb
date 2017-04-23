612140ca9b4efc91d3a920ebfd475ce5f7bc2dc0
Tony Yang <tianyuyang658@yahoo.com>
2017-04-22 18:49:34 -0700

    Move the code that manages scm mount to the install git server cookbook. Base Ubuntu cookbook should not manage the scm git repo mount

--- cookbooks/install_git_server_cookbook/attributes/default.rb
+++ cookbooks/install_git_server_cookbook/attributes/default.rb
@@ -4,3 +4,6 @@ default['gitpassword'] = ''
 default['githome'] = ::File.join('/', 'home', node['gituser'])
 default['gitrepo'] = ::File.join(node['githome'], 'repo')
 default['nfsrepo'] = ::File.join('/', 'mnt', 'scm', 'repo')
+# Enter the NFS IP address here to mount the NFS point
+default['nfs'] = 'None'
+default['nfsmount'] = ::File.join('/', 'mnt', 'scm')
