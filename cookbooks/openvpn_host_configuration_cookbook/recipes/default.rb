a9d794fe7d191f60d1669d019689f742b4291374
Tony Yang <tianyuyang658@yahoo.com>
2018-07-01 08:50:43 -0700

    Add recipe to configure host network for openvpn

--- cookbooks/openvpn_host_configuration_cookbook/recipes/default.rb
+++ cookbooks/openvpn_host_configuration_cookbook/recipes/default.rb
@@ -4,6 +4,28 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-apt_update 'update_package_info' do
-  action :update
+cookbook_file '/etc/sysctl.conf' do
+  source "sysctl.conf.#{node['platform_version']}"
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+execute 'update sysctl' do
+  command 'sysctl -p'
+end
+
+cookbook_file '/etc/default/ufw' do
+  source 'ufw'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+execute 'allow_openvpn' do
+  command 'ufw allow 1194/tcp'
+end
+
+execute 'update the ufw rule' do
+  command 'ufw reload'
 end
--- cookbooks/openvpn_host_configuration_cookbook/spec/unit/recipes/default_spec.rb
+++ cookbooks/openvpn_host_configuration_cookbook/spec/unit/recipes/default_spec.rb
@@ -17,8 +17,41 @@ describe 'openvpn_host_configuration_cookbook::default' do
       expect { chef_run }.to_not raise_error
     end
 
-    it 'updates package information' do
-      expect(chef_run).to update_apt_update('update_package_info')
+    it 'updates the sysctl conf' do
+      expect(chef_run).to create_cookbook_file('/etc/sysctl.conf').with(
+        source: 'sysctl.conf.16.04',
+        owner: 'root',
+        group: 'root',
+        mode: '0644'
+      )
+    end
+
+    it 'updates the sysctl' do
+      expect(chef_run).to run_execute('update sysctl')
+        .with(command: 'sysctl -p')
+    end
+
+    it 'enables the openvpn port' do
+      expect(chef_run).to run_execute('allow_openvpn')
+        .with(command: 'ufw allow 1194/tcp')
+    end
+
+    it 'updates the ufw rule' do
+      expect(chef_run).to run_execute('update the ufw rule')
+        .with(command: 'ufw reload')
+    end
+  end
+
+  context 'For Ubuntu 18.04 LTS platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'updates the sysctl conf' do
+      expect(chef_run).to create_cookbook_file('/etc/sysctl.conf').with(
+        source: 'sysctl.conf.18.04'
+      )
     end
   end
 end
